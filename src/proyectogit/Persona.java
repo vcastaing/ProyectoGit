d35f89276d52167033ca9899227332a80d3eb20a
valer <valer@10.50.42.116>
2022-08-01 14:57:44 -0600

    Se agregan los métodos de acceso a la clase persona

--- src/proyectogit/Persona.java
+++ src/proyectogit/Persona.java
@@ -12,5 +12,29 @@ public class Persona {
     private int codigo;
     private String nombre;
     private String direccion;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
     
 }
